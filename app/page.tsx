0906f0c4b29bcd0ee098dc686d423b676a73bba6
Mudit Kapoor <muditk441@gmail.com>
2023-12-09 20:48:52 +0530

    Added authentication with clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <div className="h1-bold">Hi I am mudit kapoor</div>
-      <div className="h2-bold">Hi I am mudit kapoor</div>
-      <div className="h3-bold">Hi I am mudit kapoor</div>
-    </div>
-  );
-}
