41297a0e1b9a4b87cd1020982b2519d82240d501
Mudit Kapoor <muditk441@gmail.com>
2023-12-11 16:15:56 +0530

    navbar done for mobile and desktop

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import "../../../app/globals.css";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50  w-full gap-5 p-6 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="Learn To Code"
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900  max-sm:hidden">
+          Learn <span className="text-primary-500">To Code</span>
+        </p>
+      </Link>
+      <GlobalSearch />
+      <div className="flex-between gap-5">
+        <Theme />
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+            afterSignOutUrl="/"
+          />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
