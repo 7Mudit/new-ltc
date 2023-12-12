838baa4b17a7e733e6444155fb2dc258f4304978
Mudit Kapoor <muditk441@gmail.com>
2023-12-12 15:40:41 +0530

    local search bar made

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,29 @@
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      {/* <UserButton afterSignOutUrl="/" /> */}
-      Hi
-    </div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
+        <h1 className="h1-bold text-dark100_light900 ">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end  max-sm:w-full ">
+          <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+            Ask Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+      </div>
+    </>
   );
 };
 
