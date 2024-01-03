c9baef070ade5609fa47518934ba9670104d4f42
Mudit Kapoor <muditk441@gmail.com>
2024-01-03 13:07:06 +0530

    badging system done

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -75,10 +75,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
         </div>
       </div>
       <Stats
-        totalQuestions={userInfo?.totalQuestions!}
-        totalAnswers={userInfo?.totalAnswers!}
-        badges={userInfo?.user.badges}
-        reputation={userInfo?.user.reputation}
+        reputation={userInfo.reputation}
+        totalQuestions={userInfo.totalQuestions}
+        totalAnswers={userInfo.totalAnswers}
+        badges={userInfo.badgeCounts}
       />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
